cd852b7a6bbb44b46a4b045bf1fbad7a31b1834a
luciana87 <ch.luciana87@gmail.com>
2023-12-26 13:06:59 -0300

    Create utils files

---
+++ angular/gestion-compras/src/app/utils/order.ts
@@ -0,0 +1,84 @@
+import { PurchaseOrder } from "../models/purchaseOrder"
+
+
+export class OrderUtils {
+
+    static initializeOrder(): PurchaseOrder {
+        return {
+            num_order: 0,
+            supplier: {
+                id: -1,
+                code: '',
+                business_name: '',
+                category: '',
+                website: '',
+                phone_number: 0,
+                email: '',
+                address: {
+                    street: '',
+                    postal_code: '',
+                    city: '',
+                    departament: '',
+                    country: ''
+                },
+                tax_data: {
+                    cuit: 0,
+                    responsible_registered: ''
+                },
+                contact: {
+                    name: '',
+                    lastname: '',
+                    phone_number: 0,
+                    email: '',
+                    rol: ''
+                },
+                logo: ''
+            },
+            product: {
+                id: -1,
+                sku: '',
+                supplier: {
+                    id: 0,
+                    code: '',
+                    business_name: '',
+                    category: '',
+                    website: '',
+                    phone_number: 0,
+                    email: '',
+                    address: {
+                        street: '',
+                        postal_code: '',
+                        city: '',
+                        departament: '',
+                        country: ''
+                    },
+                    tax_data: {
+                        cuit: 0,
+                        responsible_registered: ''
+                    },
+                    contact: {
+                        name: '',
+                        lastname: '',
+                        phone_number: 0,
+                        email: '',
+                        rol: ''
+                    },
+                    logo: ''
+                },
+                category: {
+                    id: 0,
+                    name: ''
+                },
+                name: '',
+                description: '',
+                price: 0,
+                image: ''
+            },
+            issue_date: new Date,
+            deadline: new Date,
+            price: 0,
+            amount: 0,
+            subtotal: 0
+        }
+    }
+}
